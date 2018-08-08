1082bc7115c31ede2c5161efa2bbf91db5652ddd
cstonic <scissorfists@gmail.com>
2018-08-08 19:56:12 -0400

    Added price to 524 in data.json

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import "rxjs/add/operator/map";
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private http: Http, public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.http.get('/assets/data.json')
+      .map(response => response.json())
+      .subscribe(data => console.log(data));
   }
 
 }
